4d2bf6bd3da412f16536d103f14e03dd5bef3348
eskaliber00 <eskaliber00@gmail.com>
2021-10-07 14:20:03 +0700

    add migration

---
+++ database/migrations/2021_10_06_100834_create_transaction_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('transactionHeader_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+
+            $table->foreign('transactionHeader_id')->references('id')->on('carts')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
