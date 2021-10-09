b04ccfd6178e8cdcd9b0882f1e7a68122b44378c
eskaliber00 <eskaliber00@gmail.com>
2021-10-09 18:49:34 +0700

    add seeder + update migrate

--- database/migrations/2021_10_06_100753_create_transaction_headers_table.php
+++ database/migrations/2021_10_06_100753_create_transaction_headers_table.php
@@ -16,6 +16,7 @@ class CreateTransactionHeadersTable extends Migration
         Schema::create('transaction_headers', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('status');
 
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
